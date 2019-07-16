3fa049a4984913ad094c3a221ff24b5e73a491ed
Сергей Вилков <btc.devel@gmail.com>
2019-07-17 00:35:11 +0300

    Icons

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,19 @@ console.log('there is');
 
 jQuery(function() {
 
-  let $navListItem = $('.b-nav__li');
+  let $navListItem  = $('.b-nav__li');
+  let $navCloseIcon = $('.b-nav__dropdown-menu-close');
 
   $navListItem.on('click', function() {
     console.log( $(this).data('nav-item') )
+    $navListItem.removeClass('b-nav__li_state-active');
+    $(this).addClass('b-nav__li_state-active');
     $('.b-nav__dropdown-block').removeClass('b-nav__dropdown-block_state-active');
     $( '[data-dropdown="' + $( this ).data('nav-item') + '"]' ).addClass('b-nav__dropdown-block_state-active');
+  });
+
+  $navCloseIcon.on('click', function() {
+    $(this).parents('.b-nav__dropdown-block').removeClass('b-nav__dropdown-block_state-active');
   });
 
 });
